322f06ae2212ed7d699cb36135e439a68be3e58c
chirchir <chirchir7370@gmail.com>
2020-03-09 15:19:57 +0300

    added about, contact and treatment page

---
+++ about.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <?php  require './base/head.html'; ?>
+  </head>
+  <body>
+	<?php require './base/navbar.html'; ?>
+    <!-- END nav -->
+    
+    <section class="hero-wrap hero-wrap-2" style="background-image: url('images/bg_1.jpg');">
+      <div class="overlay"></div>
+      <div class="container">
+        <div class="row no-gutters slider-text align-items-center justify-content-center">
+          <div class="col-md-9 ftco-animate text-center">
+            <h1 class="mb-2 bread">About Us</h1>
+            <p class="breadcrumbs"><span class="mr-2"><a href="index.html">Home <i class="ion-ios-arrow-forward"></i></a></span> <span>About us <i class="ion-ios-arrow-forward"></i></span></p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="ftco-section ftco-about ftco-no-pt ftco-no-pb ftco-counter" id="section-counter">
+			<div class="container consult-wrap">
+				<div class="row d-flex align-items-stretch">
+					<div class="col-md-6 wrap-about align-items-stretch d-flex">
+						<div class="img" style="background-image: url(images/about.jpg);"></div>
+					</div>
+					<div class="col-md-6 wrap-about ftco-animate py-md-5 pl-md-5">
+						<div class="heading-section mb-4">
+							<span class="subheading">Welcome to St Nicholas Rehabalitation Center</span>
+							
+						</div>
+						<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+						<div class="tabulation-2 mt-4">
+							<ul class="nav nav-pills nav-fill d-md-flex d-block">
+							  <li class="nav-item">
+							    <a class="nav-link active py-2" data-toggle="tab" href="#home1"><span class="ion-ios-home mr-2"></span> Our Mission</a>
+							  </li>
+							  <li class="nav-item px-lg-2">
+							    <a class="nav-link py-2" data-toggle="tab" href="#home2"><span class="ion-ios-person mr-2"></span> Our Vision</a>
+							  </li>
+							  <li class="nav-item">
+							    <a class="nav-link py-2" data-toggle="tab" href="#home3"><span class="ion-ios-mail mr-2"></span> Our Value</a>
+							  </li>
+							</ul>
+							<div class="tab-content bg-light rounded mt-2">
+							  <div class="tab-pane container p-0 active" id="home1">
+							  	<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
+							  </div>
+							  <div class="tab-pane container p-0 fade" id="home2">
+							  	<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
+							  </div>
+							  <div class="tab-pane container p-0 fade" id="home3">
+							  	<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
+							  </div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+
+    
+		
+    <!-- footer -->
+   <?php require './base/footer.html'; ?>
+    
+  
+
+  <!-- loader -->
+  <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
+
+
+  <script src="js/jquery.min.js"></script>
+  <script src="js/jquery-migrate-3.0.1.min.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/jquery.easing.1.3.js"></script>
+  <script src="js/jquery.waypoints.min.js"></script>
+  <script src="js/jquery.stellar.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/jquery.magnific-popup.min.js"></script>
+  <script src="js/aos.js"></script>
+  <script src="js/jquery.animateNumber.min.js"></script>
+  <script src="js/scrollax.min.js"></script>
+  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
+  <script src="js/google-map.js"></script>
+  <script src="js/main.js"></script>
+    
+  </body>
+</html>
---
+++ events.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Treatments</title>
+    <?php  require './base/head.html';?>
+  </head>
+  <body>
+	 <?php require './base/navbar.html'; ?>
+    <!-- END nav -->
+    
+    <section class="hero-wrap hero-wrap-2" style="background-image: url('images/bg_1.jpg');">
+      <div class="overlay"></div>
+      <div class="container">
+        <div class="row no-gutters slider-text align-items-center justify-content-center">
+          <div class="col-md-9 ftco-animate text-center">
+            <h1 class="mb-2 bread">Events</h1>
+            <p class="breadcrumbs"><span class="mr-2"><a href="index.html">Home <i class="ion-ios-arrow-forward"></i></a></span> <span>Events <i class="ion-ios-arrow-forward"></i></span></p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+    <section class="ftco-section">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-4 d-flex">
+						<div class="services-2 border rounded text-center ftco-animate">
+							<div class="icon mt-2 d-flex justify-content-center align-items-center"><span class="flaticon-analysis"></span></div>
+							<div class="text media-body">
+								<h3>Event 1</h3>
+								<p>Far far away, behind the word mountains, far from the countries Vokalia.</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-4 d-flex">
+						<div class="services-2 border rounded text-center ftco-animate">
+							<div class="icon mt-2 d-flex justify-content-center align-items-center"><span class="flaticon-business"></span></div>
+							<div class="text media-body">
+								<h3>Event 1</h3>
+								<p>Far far away, behind the word mountains, far from the countries Vokalia.</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-4 d-flex">
+						<div class="services-2 border rounded text-center ftco-animate">
+							<div class="icon mt-2 d-flex justify-content-center align-items-center"><span class="flaticon-insurance"></span></div>
+							<div class="text media-body">
+								<h3>Event 1</h3>
+								<p>Far far away, behind the word mountains, far from the countries Vokalia.</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-4 d-flex">
+						<div class="services-2 border rounded text-center ftco-animate">
+							<div class="icon mt-2 d-flex justify-content-center align-items-center"><span class="flaticon-money"></span></div>
+							<div class="text media-body">
+								<h3>Event 1</h3>
+								<p>Far far away, behind the word mountains, far from the countries Vokalia.</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-4 d-flex">
+						<div class="services-2 border rounded text-center ftco-animate">
+							<div class="icon mt-2 d-flex justify-content-center align-items-center"><span class="flaticon-rating"></span></div>
+							<div class="text media-body">
+								<h3>Event 1</h3>
+								<p>Far far away, behind the word mountains, far from the countries Vokalia.</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-4 d-flex">
+						<div class="services-2 border rounded text-center ftco-animate">
+							<div class="icon mt-2 d-flex justify-content-center align-items-center"><span class="flaticon-search-engine"></span></div>
+							<div class="text media-body">
+								<h3>Event 1</h3>
+								<p>Far far away, behind the word mountains, far from the countries Vokalia.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		
+   <?php require './base/footer.html' ?>
+    
+  
+
+  <!-- loader -->
+  <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
+
+
+  <script src="js/jquery.min.js"></script>
+  <script src="js/jquery-migrate-3.0.1.min.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/jquery.easing.1.3.js"></script>
+  <script src="js/jquery.waypoints.min.js"></script>
+  <script src="js/jquery.stellar.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/jquery.magnific-popup.min.js"></script>
+  <script src="js/aos.js"></script>
+  <script src="js/jquery.animateNumber.min.js"></script>
+  <script src="js/scrollax.min.js"></script>
+  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
+  <script src="js/google-map.js"></script>
+  <script src="js/main.js"></script>
+    
+  </body>
+</html>
