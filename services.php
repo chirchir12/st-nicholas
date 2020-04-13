8365667c6dee9b773acfb7fb7e7770d07cf33045
chirchir <chirchir7370@gmail.com>
2020-04-13 13:49:34 +0300

    added services page and its content

--- services.php
+++ services.php
@@ -18,69 +18,138 @@
           </div>
         </div>
       </div>
-    </section>
+	</section>
+	
+	<section class="service-container">
+		<div class="container">
+			<h3>Our Services</h3>
+			<div class="rehabilitation-container">
+				<h5>Rehabilitation</h5>
+				<p>This entails the restoration of normal life after addiction recovery, long-term or incapacitating illness, accidents or imprisonment. At St Nicholas Rehabilitation and Nursing Home, we offer the following rehabilitation; physiotherapy, occupational therapy and speech therapy.</p>
+				<h6>Drug Rehabilitation</h6>
+				<p>We specialize in drug rehabilitation which is the process of medical or psychotherapeutic treatment for dependence on psychoactive substances and street drugs.</p>
+				<div class="Psychoactive">
+					<span>Psychoactive substance includes;</span>
+					<ul>
+						<li>Alcohol</li>
+						<li>Prescription drugs</li>
+					</ul>
+				</div>
+				<div class="Street-drugs">
+					<span>Street drugs</span>
+					<ul>
+						<li>Cannabis (marijuana/bhang)</li>
+						<li>Heroin</li>
+						<li>Cocaine</li>
+						<li>Amphetamines</li>
+					</ul>
+				</div>
+				<p>We help the patient seize substance abuse to avoid psychological, legal financial, social and physical consequences that can be caused by extreme abuse. We also help the patient to overcome various psychological disorders such as</p>
+				<ol>
+					<li>Anxiety disorders.</li>
+					<li>Depression, bipolar disorders and other mood disorder.</li>
+					<li>Eating disorder.</li>
+					<li>Personality disorder.</li>
+					<li>Post-traumatic stress disorder.</li>
+					<li>Psychotic disorder.</li>
+					<li>Antisocial disorder.</li>
+				</ol>
+				<div class="stages-rehabilitation">
+					<h6>STAGES OF REHABILITATION PROCESS: INCEPTION TO COMPLETION.</h6>
+					<ul>
+						<li><span>Orientation and application-</span>This is where the intended client is familiarized with the whole process and given guidance on how to enroll to the rehabilitation, the intended client is also given information on the application requirement to apply</li>
+						<li><span>Intake appointment-</span>This is the  interview process that serves to admit that there is a substance that has been abused</li>
+						<li><span>Eligibility determination-</span>This a process where we determine if someone is really qualified for the services being offered</li>
+						<li><span>Admission and assessment-</span>At this stage a record of a leader's complaint and history is provided by the client, physical examination is done then a complete psychological evaluation is carried out.</li>
+						<li><span>Co-occurring disorder and dual diagnosis-</span>this is where symptoms predated a specific mental disorder and substance abuse or any kind of addiction is diagnosed such includes depression ,anxiety and others</li>
+						<li><span>Individual treatment plan-</span>This is where the course of treatment is outlined and defined, it delineates the goals, objectives and resources of an individual</li>
+						<li><span>Detoxification- </span>this is the process of removing addictive substances from the body under medical supervision</li>
+						<li><span>Treatment plan-</span>At this stage a detailed treatment plan is given to individual patient and the they are engaged in the treatment process, the plan includes clients information, diagnosis  prescriptions and measurement objectives</li>
+						<li>Inpatient outpatient and partial hospitalization.</li>
+						<li><span>Therapy –</span>this includes; rehab therapy, individual therapy, family therapy and occupational therapy.</li>
+						<li><span>Employment services-</span>at our rehabilitation we try to avoid relapse at all cost hence we offer try to find what the clients can do after finishing the previous rehabilitation processes through providing them with employment directly or indirectly or financing their talents and dreams also businesses enterprises, at this stage we partner with riti association a project of the rehabilitation and nursing center for best results</li>
+						<li><span>Aftercare services-</span>This a follow up treatment stage to ensure an individual doesn't relapse and stay focused on his or her recovery, this is done by  group counseling, advising on sober living, being joined to a support group.</li>
 
 
-    <section class="ftco-section">
-			<div class="container">
-				<div class="row">
-					<div class="col-lg-4 d-flex">
-						<div class="services-2 border rounded text-center ftco-animate">
-							<div class="icon mt-2 d-flex justify-content-center align-items-center"><span class="flaticon-analysis"></span></div>
-							<div class="text media-body">
-								<h3>Business Analysis</h3>
-								<p>Far far away, behind the word mountains, far from the countries Vokalia.</p>
-							</div>
-						</div>
-					</div>
-					<div class="col-lg-4 d-flex">
-						<div class="services-2 border rounded text-center ftco-animate">
-							<div class="icon mt-2 d-flex justify-content-center align-items-center"><span class="flaticon-business"></span></div>
-							<div class="text media-body">
-								<h3>Business Consulting</h3>
-								<p>Far far away, behind the word mountains, far from the countries Vokalia.</p>
-							</div>
-						</div>
-					</div>
-					<div class="col-lg-4 d-flex">
-						<div class="services-2 border rounded text-center ftco-animate">
-							<div class="icon mt-2 d-flex justify-content-center align-items-center"><span class="flaticon-insurance"></span></div>
-							<div class="text media-body">
-								<h3>Business Insurance</h3>
-								<p>Far far away, behind the word mountains, far from the countries Vokalia.</p>
-							</div>
-						</div>
-					</div>
-					<div class="col-lg-4 d-flex">
-						<div class="services-2 border rounded text-center ftco-animate">
-							<div class="icon mt-2 d-flex justify-content-center align-items-center"><span class="flaticon-money"></span></div>
-							<div class="text media-body">
-								<h3>Global Investigation</h3>
-								<p>Far far away, behind the word mountains, far from the countries Vokalia.</p>
-							</div>
-						</div>
-					</div>
-					<div class="col-lg-4 d-flex">
-						<div class="services-2 border rounded text-center ftco-animate">
-							<div class="icon mt-2 d-flex justify-content-center align-items-center"><span class="flaticon-rating"></span></div>
-							<div class="text media-body">
-								<h3>Audit &amp; Evaluation</h3>
-								<p>Far far away, behind the word mountains, far from the countries Vokalia.</p>
-							</div>
-						</div>
-					</div>
-					<div class="col-lg-4 d-flex">
-						<div class="services-2 border rounded text-center ftco-animate">
-							<div class="icon mt-2 d-flex justify-content-center align-items-center"><span class="flaticon-search-engine"></span></div>
-							<div class="text media-body">
-								<h3>Marketing Strategy</h3>
-								<p>Far far away, behind the word mountains, far from the countries Vokalia.</p>
-							</div>
-						</div>
-					</div>
+					</ul>
 				</div>
 			</div>
-		</section>
+			<div class="nursing-services">
+				<h5>NURSING SERVICE</h5>
+				<p>We have a skilled and knowledgeable personnel consisting of nursing aides that provide the best medical care. Our nursing home operates daily on a 24 hour system. The facility also offers hospice care for the critically ill, patients with chronic sickness, HIV/Aids or the disabled as well as custodial care for routine tasks such as bathing, getting dressed, eating as so on. </p>
+				<div class="other-services">
+					<span>Other important services offered are as follows;</span>
+					<ul>
+						<li>Room and board- this is where meals, home stays and medical care is offered around the clock</li>
+						<li>Monitoring and education a patient activities are monitored closely hence give proper advice to help in recovery</li>
+						<li>Personal care</li>
+						<li>24hours emergency care</li>
+						<li>Social and recreational activities such as games, entertainment that includes watching motivational and educational movies, music that create positive impact to a client, group talks to encourage each other library and others</li>
+						<li>Health related issues such as chronic pains diabetes, irritable bowel syndrome, weight</li>
+					</ul>
+				</div>
+			</div>
+			<div class="counselling-services">
+				<h5>COUNSELING SERVICES</h5>
+				<p>This service involves counseling on the 12 steps to recovery. There are various types of counseling offered which includes; addiction counseling ,child counseling, family counseling and psychological counseling, mental health counseling , substance abuse counseling education counseling and marriage counseling.</p>
+				<div class="stages-in-counselling">
+					<span>The experts on counseling are keen in following the stages of change which are;</span>
+					<ul>
+						<li><span>Pre-contemplation - </span>This is where the patients has not yet come to terms with the fact that he or she needs help, it usually takes 6 months, the patient is unaware of their problematic behaviors the counselor hence encourage the reevaluation of the current behavior and also self -exploration ,the patient is given a clear picture of how their behavior is harmful</li>
+						<li><span>Contemplation - </span>This is where the pros and cons of changing becomes known to the patient hence he or she focuses on changing their behaviors. the counselor hence encourage and promote positive outcome to be expected</li>
+						<li><span>Action -</span>This is where the patient practices the new behavior in a given period of time usually between 3 to 6 months, the counselor encourages social support, bolsters self-efficiency for dealing with obstacles, help the client to combat their feelings of loss and reinstate long term benefits</li>
+						<li><span>Maintenance -</span>This is where the client has made an overt modifications in their lifestyles and are working to avoid relapse, the counselor make a follow up the client how to cope up with relapse, reinforce internal rewards and plan way forward after counseling sessions are over, this can take up to 5years</li>
+						<li><span>After care -</span>The counselor evaluate the triggers of relapse, reassesses motivation and barriers  to successful full recovery and plan stronger coping strategies</li>
+					</ul>
+				</div>
+			</div>
+			<div class="psychiatric-services">
+				<h5>PSYCHIATRIC SERVICES</h5>
+				<p>We deal with diagnosis prevention study, treatment and management of mental disorders such mental disorders include</p>
+				<div class="Psychological-disorders">
+					<h6>Psychological disorders</h6>
+					<p>Examples are; adjustment disorder anxiety, Bipolar, Depression, obsessive compulsive disorder, panic disorder, post –traumatic disorder, phobia/fear, somatic symptom disorder and insomnia.</p>
+				</div>
+				<div class="Addiction-disorder">
+					<h6>Addiction disorder</h6>
+					<p>This is a brain disease manifested by obsessive substance use apart from its harmful concerns, the substances abused includes alcohol, prescription drugs, cocaine, heroin marijuana or bhang and so on, the symptoms of substance behavior may include strong craving to use the substance being used, social problems where one loses focus on important societal activities such as schooling or drug withdrawal symptoms.</p>
+				
+				</div>
+				<div class="Neurocognitive-disorders">
+					<h6>Neurocognitive disorders</h6>
+					<p>Neurocognitive disorders such as dementia, stroke, traumatic brain injury, schizophrenia borderline personality disorders and Autism spectrum disorder</p>
+				</div>
+				<div class="other-dissorders">
+					<h6>Other Disorders</h6>
+					<li>Other psychological disorders which are anger management issues, grief and bereavement, relationship issues. Stress and burnout.</li>
+				</div>
+			</div>
+			<div class="vct-services">
+				<h5>V.C.T Services</h5>
+				<p>Our specialist are skilled and knowledgeable on how to counsel and carry out HIV/AIDS tests</p>
+			</div>
+			<div class="counselling-psychology">
+				<h5>SCHOOL OF COUNSELLING PSYCHOLOGY</h5>
+				<p>Counseling psychology is a psychological specialty within a professional psychology that maintains a focus on facilitating personal and interpersonal functioning across the lifespan of an individual. At St Nicholas rehabilitation and nursing home we are designed to offer this course and majorly dealing with addiction counseling to come with the best counseling psychologist who can work well with individual groups to improve their mental health, encourage clients to discuss their issues, examine issue to do with substance abuse and alcoholism, bullying, depression and many other psychological issues and much more.</p>
+
+			</div>
+			<div class="education">
+				<h5>SECONDARY EDUCATION AND INDUSTRIAL TRAINING INSTITUTE</h5>
+				<div class="technical-training">
+					<h6>Technical Training</h6>
+					<p>Technical training-this training is offered to our clients only to help them gain skills and knowledge with will in turn increase their employability tendency hence avoiding relapse</p>
+				</div>
+				<div class="secondary-education">
+					<h6>Secondary Education</h6>
+					<p>Secondary education-this is also offered to our clients especially those who dropped out of school ,this reassures the client of their continuity of education during the rehabilitation process.</p>
+				</div>
+			</div>
+
+		</div>
+	</section>
+
+
+  
 
 		
    <?php require './base/footer.html' ?>
