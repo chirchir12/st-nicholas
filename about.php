94bec533a93fa3bc9eaba39005667bb328ea9992
chirchir <chirchir7370@gmail.com>
2020-05-05 15:14:54 +0300

    fixed errorsin about.page

--- about.php
+++ about.php
@@ -1,99 +1,125 @@
-<?php 
+<?php
 
-$page = basename(__FILE__)
+$page = basename(__FILE__);
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <?php  require './base/head.html'; ?>
-  </head>
-  <body>
-	<?php require './base/navbar.php'; ?>
+
+<head>
+    <?php require './base/head.html'; ?>
+</head>
+
+<body>
+    <?php require './base/navbar.php'; ?>
     <!-- END nav -->
-    
+
     <section class="hero-wrap hero-wrap-2" style="background-image: url('images/rehab2.jpeg');">
-      <div class="overlay"></div>
-      <div class="container">
-        <div class="row no-gutters slider-text align-items-center justify-content-center">
-          <div class="col-md-9 ftco-animate text-center">
-            <h1 class="mb-2 bread">About Us</h1>
-            <p class="breadcrumbs"><span class="mr-2"><a href="index.html">Home <i class="ion-ios-arrow-forward"></i></a></span> <span>About us <i class="ion-ios-arrow-forward"></i></span></p>
-          </div>
+        <div class="overlay"></div>
+        <div class="container">
+            <div class="row no-gutters slider-text align-items-center justify-content-center">
+                <div class="col-md-9 ftco-animate text-center">
+                    <h1 class="mb-2 bread">About Us</h1>
+                    <p class="breadcrumbs"><span class="mr-2"><a href="index.html">Home <i
+                                    class="ion-ios-arrow-forward"></i></a></span> <span>About us <i
+                                class="ion-ios-arrow-forward"></i></span></p>
+                </div>
+            </div>
         </div>
-      </div>
     </section>
 
-    <section class="container">
-		<div class="title">
-      <h1>Executive Summary</h1>
-      <?php echo $page ?>
-		</div>
-		<div class="st-nicholas-description">
-			<p>The institute has been initiated to address alcoholism and drug abuse, cutting across all age groups. In the present day era, unemployment and financial constraints are major issues as per United Nations figures with an estimation of 87% of the world population living below the dollar per day. This further demonstrates poverty, hunger, lack of education, maternal mortality and unemployment have a greater significance and impact. </p>
-			<p>While the government has made strides to provide access to education there is a huge gap when it comes to equal access to employment opportunities. Therefore due to high unemployment, many people turn to alcoholism and drug abuse, robbery and gang violence, among others vices. At St Nicholas Rehabilitation Center and Nursing Home, we have recognized the need to help the community counter drug addiction through quality rehabilitation services. The institution will provide other important services such as guidance and counseling, training on employability skills and pitch opportunities within and outside the country.
-</p>
-		</div>
-		<div class="mission-vission-core">
-			<h3>Mission</h3>
-			<p>To lead in the fight against alcohol and drug abuse through prevention treatment, rehabilitation and offering training on employability skills as a follow up to the affected adults and youths.</p>
-			<h3>Vision</h3>
-			<p>A society free from alcohol and drug abuse trained in employability skills.</p>
-			<h3>Core Values</h3>
-			<ul>
-				<li>Self-discipline</li>
-				<li>Consistency</li>
-				<li>Positivity</li>
-				<li>Passion</li>
-				<li>Education</li>
-				<li>Patriotism</li>
-				<li>Respect</li>
-			</ul>
-		</div>
-		<div class="objectives">
-			<h3>Objectives</h3>
-			<ol>
-				<li>To rehabilitate adults and youth addicted to alcohol and drug abuse.</li>
-				<li>To educate the youth and adults on alcohol and drug abuse to prevent more culprits.</li>
-				<li>To guide and counsel youth on alcoholism and drug abuse.</li>
-				<li>To create a society free from alcohol and drug abuse.</li>
-				<li>To help the youth to be self-reliant.</li>
-				<li>To train adults and youth on employability skills.</li>
-				<li>To employ youths through manufacturing items produced during training.</li>
+    <section class="container mt-4">
+        <div class="row">
+            <div class="col-12 col-md-6">
+                <div class="title">
+                    <h1>Executive Summary</h1>
+                </div>
+                <div class="st-nicholas-description">
+                    <p>The institute has been initiated to address alcoholism and drug abuse, cutting across all age
+                        groups. In the present day era, unemployment and financial constraints are major issues as per
+                        United Nations figures with an estimation of 87% of the world population living below the dollar
+                        per day. This further demonstrates poverty, hunger, lack of education, maternal mortality and
+                        unemployment have a greater significance and impact. </p>
+                    <p>While the government has made strides to provide access to education there is a huge gap when it
+                        comes to equal access to employment opportunities. Therefore due to high unemployment, many
+                        people turn to alcoholism and drug abuse, robbery and gang violence, among others vices. At St
+                        Nicholas Rehabilitation Center and Nursing Home, we have recognized the need to help the
+                        community counter drug addiction through quality rehabilitation services. The institution will
+                        provide other important services such as guidance and counseling, training on employability
+                        skills and pitch opportunities within and outside the country.
+                    </p>
+                </div>
+            </div>
+            <div class="col-12 col-md-6 pt-5">
+                <img src="images/rehab1.jpeg" class="img-fluid" alt="">
+            </div>
+        </div>
+        <div class="dropdown-divider"></div>
+        <div class="mission-vission-core">
+            <h3>Mission</h3>
+            <p>To lead in the fight against alcohol and drug abuse through prevention treatment, rehabilitation and
+                offering training on employability skills as a follow up to the affected adults and youths.</p>
+            <h3>Vision</h3>
+            <p>A society free from alcohol and drug abuse trained in employability skills.</p>
+            <h3>Core Values</h3>
+            <ul>
+                <li>Self-discipline</li>
+                <li>Consistency</li>
+                <li>Positivity</li>
+                <li>Passion</li>
+                <li>Education</li>
+                <li>Patriotism</li>
+                <li>Respect</li>
+            </ul>
+        </div>
+        <div class="objectives">
+            <h3>Objectives</h3>
+            <ol>
+                <li>To rehabilitate adults and youth addicted to alcohol and drug abuse.</li>
+                <li>To educate the youth and adults on alcohol and drug abuse to prevent more culprits.</li>
+                <li>To guide and counsel youth on alcoholism and drug abuse.</li>
+                <li>To create a society free from alcohol and drug abuse.</li>
+                <li>To help the youth to be self-reliant.</li>
+                <li>To train adults and youth on employability skills.</li>
 
-			</ol>
-		</div>
+            </ol>
+        </div>
 
-	</section>
+    </section>
 
 
-    
-		
+
+
     <!-- footer -->
-   <?php require './base/footer.html'; ?>
-    
-  
-
-  <!-- loader -->
-  <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
+    <?php require './base/footer.html'; ?>
 
 
-  <script src="js/jquery.min.js"></script>
-  <script src="js/jquery-migrate-3.0.1.min.js"></script>
-  <script src="js/popper.min.js"></script>
-  <script src="js/bootstrap.min.js"></script>
-  <script src="js/jquery.easing.1.3.js"></script>
-  <script src="js/jquery.waypoints.min.js"></script>
-  <script src="js/jquery.stellar.min.js"></script>
-  <script src="js/owl.carousel.min.js"></script>
-  <script src="js/jquery.magnific-popup.min.js"></script>
-  <script src="js/aos.js"></script>
-  <script src="js/jquery.animateNumber.min.js"></script>
-  <script src="js/scrollax.min.js"></script>
-  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
-  <script src="js/google-map.js"></script>
-  <script src="js/main.js"></script>
-    
-  </body>
+
+    <!-- loader -->
+    <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px">
+            <circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee" />
+            <circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10"
+                stroke="#F96D00" /></svg></div>
+
+
+    <script src="js/jquery.min.js"></script>
+    <script src="js/jquery-migrate-3.0.1.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.easing.1.3.js"></script>
+    <script src="js/jquery.waypoints.min.js"></script>
+    <script src="js/jquery.stellar.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/jquery.magnific-popup.min.js"></script>
+    <script src="js/aos.js"></script>
+    <script src="js/jquery.animateNumber.min.js"></script>
+    <script src="js/scrollax.min.js"></script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false">
+    </script>
+    <script src="js/google-map.js"></script>
+    <script src="js/main.js"></script>
+
+</body>
+
 </html>
